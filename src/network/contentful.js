dd5afc4432f8d225aca1be514763bab4841bc57e
msaljuk <msaljuk@stanford.edu>
2020-09-16 16:32:36 +0500

    Update contentful.js

--- src/network/contentful.js
+++ src/network/contentful.js
@@ -1,11 +1,11 @@
-const contentful = require("contentful");
+// const contentful = require("contentful");
 
-const client = contentful.createClient({
-  space: "developer_bookshelf",
-  accessToken: "0b7f6x59a0",
-});
+// const client = contentful.createClient({
+//   space: "developer_bookshelf",
+//   accessToken: "0b7f6x59a0",
+// });
 
-client
-  .getEntry("5PeGS2SoZGSa4GuiQsigQu")
-  .then((entry) => console.log(entry))
-  .catch((err) => console.log(err));
+// client
+//   .getEntry("5PeGS2SoZGSa4GuiQsigQu")
+//   .then((entry) => console.log(entry))
+//   .catch((err) => console.log(err));
